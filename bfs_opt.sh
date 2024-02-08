a8b0b57c2196256010a0a86a8c6f4519660842e4
rashid <agp.rashid@gmail.com>
2024-02-08 17:32:46 +0100

    L2 cache writing has problem. When there is load in L2 but there is no SS_OO state. In that case DIR is in M state but there is no data in the L2 cache

--- bfs_opt.sh
+++ bfs_opt.sh
@@ -1,13 +1,18 @@
-gdb --args ./build/X86/gem5.debug \
-    --outdir=/home/rashid/warm_result/gapbs/kron/bfs_prefetch \
+./build/X86/gem5.opt \
+    --debug-flags=RubySlicc,RubyCache,RubyGenerated \
+    --outdir=/home/rashid/warm_result/gapbs/kron/bfs_perfect \
+    --redirect-stdout \
+    --redirect-stderr \
     ./configs/deprecated/example/fs.py \
-    --checkpoint-dir=/home/rashid/uppmaxfs/rashid_files/gapbs_checkpoints/bfs \
+    --checkpoint-dir=/home/rashid/uppmaxfs/rashid_files/gapbs_checkpoints/bfs -r 1\
     -r 1\
     --kernel=/home/rashid/vmlinux/vmlinux-5.4.49 \
     --disk-image=/home/rashid/fs_img/gapbs_kron.img \
     --cpu-type=DerivO3CPU \
     --restore-with-cpu=DerivO3CPU \
     --mem-size=16GB \
+    --mem-type=DRAMsim3\
+    --network=garnet\
     --ruby \
     --caches \
     --l1d_size=64kB \
@@ -20,7 +25,7 @@ gdb --args ./build/X86/gem5.debug \
     --mesh-rows=1 \
     --l2_size=2MB \
     --l2_assoc=16 \
-    --cpu-clock=1GHz \
+    --cpu-clock=2GHz \
     --cacheline_size=64 \
-    -I 250000000
+    -I 200000000
 # -I 500000000
